be84f6a5ece33a4b692967d66fbc02658a7ed0ab
paragkulkarni <paragkk80@gmail.com>
2022-08-04 22:41:17 +0530

    morgan added successfully

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 
 const port = 5000;
 const app = express();
 
+
+app.use(morgan('dev'));
 
 app.get('/',(req, res) => {
     res.send('Hello World!');
